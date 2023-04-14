8c972c6f08d0302bff65d75594ebf67c8d2eab16
Tomas Sanhueza <tomisanhues2@gmail.com>
2023-04-14 18:38:23 -0400

    first commit

---
+++ src/main/java/me/tomisanhues2/bantrial/Ban.java
@@ -0,0 +1,22 @@
+package me.tomisanhues2.bantrial;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ban extends JavaPlugin {
+
+    private static Ban instance;
+
+    {
+        instance = this;
+    }
+
+    public static Ban getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}

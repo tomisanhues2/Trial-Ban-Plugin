3052dfa11a0069338fd7870965184d9e2792fe95
Tomas Sanhueza <tomisanhues2@gmail.com>
2023-04-15 23:11:20 -0400

    Almost done, 95%. Forgot to explain commits

--- src/main/java/me/tomisanhues2/bantrial/commands/BanCommand.java
+++ src/main/java/me/tomisanhues2/bantrial/commands/BanCommand.java
@@ -1,11 +1,13 @@
 package me.tomisanhues2.bantrial.commands;
 
 import me.tomisanhues2.bantrial.Ban;
+import me.tomisanhues2.bantrial.utils.BanType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BanCommand implements TabExecutor {
@@ -22,7 +24,7 @@ public class BanCommand implements TabExecutor {
             return true;
         }
 
-        if (!command.getName().equalsIgnoreCase("ban") || !command.getName().equalsIgnoreCase("tempban")) {
+        if (!command.getName().equalsIgnoreCase("ban")) {
             return true;
         }
 
@@ -30,9 +32,10 @@ public class BanCommand implements TabExecutor {
             commandSender.sendMessage(plugin.messages.getMessage("no-permission"));
             return true;
         }
+        System.out.println("Strings: " + Arrays.toString(strings));
 
-        if (strings.length < 3) {
-            commandSender.sendMessage(plugin.messages.getMessage("ban-usage"));active_bans
+        if (strings.length == 0) {
+            commandSender.sendMessage(plugin.messages.getMessage("ban-usage"));
             return true;
         }
         //todo: Ban logic
@@ -40,7 +43,26 @@ public class BanCommand implements TabExecutor {
         if (target == null) {
             commandSender.sendMessage(plugin.messages.getMessage("player-not-found"));
             return true;
-        }g
+        }
+        if (plugin.database.isPlayerActiveBan(target.getUniqueId())) {
+            commandSender.sendMessage(plugin.messages.getMessage("player-already-banned"));
+            return true;
+        }
+        if (strings.length == 1) {
+            plugin.banManager.addBan(target.getUniqueId(), "You have been banned!", BanType.PERM_BAN.toString(), ((Player) commandSender).getUniqueId());
+            return true;
+        }
+        String arg1 = strings[1];
+        boolean hasDuration = Character.isDigit(arg1.charAt(0));
+        String duration = BanType.PERM_BAN.toString();
+        String reason = String.join(" ", Arrays.copyOfRange(strings, 1, strings.length));
+
+        //Find out if arg1 is a time or a reason
+        if (hasDuration) {
+            duration = arg1;
+            reason = String.join(" ", Arrays.copyOfRange(strings, 2, strings.length));
+        }
+        plugin.banManager.addBan(target.getUniqueId(), reason, duration, ((Player) commandSender).getUniqueId());
 
         return true;
     }
